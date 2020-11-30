976aec406a6131d43a7c737bf0a882d4b37401f7
Nick Snyder <nsnyder1992@gmail.com>
2020-11-29 19:28:57 -0500

    updated links and projects sections. added footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,70 @@
 $(document).ready(function () {
   //Open about dropdown close others
-  $("#game-start").click(function () {
+  $("#game-start").click(() => {
     $(".history").removeClass("show");
     $(".game").addClass("show");
   });
 
-  $("#go-back").click(function () {
+  $("#go-back").click(() => {
     $(".history").addClass("show");
     $(".game").removeClass("show");
   });
 
-  function resizeDiv() {
-    let maxHeight = 0;
+  //scroll to certain technology specific project
+  $(".nav-icon").click((e) => {
+    if (e.target.id == "python") {
+      thumbnailSelect($("#fuji-thumbnail"), $("#fuji-content"));
+    } else if (e.target.id == "css") {
+      thumbnailSelect($("#creature-thumbnail"), $("#creature-content"));
+    } else if (e.target.id == "html") {
+      thumbnailSelect($("#static-thumbnail"), $("#static-content"));
+    } else if (e.target.id == "js") {
+      thumbnailSelect($("#game-thumbnail"), $("#game-content"));
+    }
+  });
 
-    $("div.max-size").each(function (index) {
-      if ($(this).height() > maxHeight) {
-        maxHeight = $(this).height();
+  //open project descriptions
+  $(".creature-thumbnail").click(() => {
+    thumbnailSelect($("#creature-thumbnail"), $("#creature-content"));
+  });
+
+  $("#fuji-thumbnail").click(() => {
+    thumbnailSelect($("#fuji-thumbnail"), $("#fuji-content"));
+  });
+
+  $(".static-thumbnail").click(() => {
+    thumbnailSelect($("#static-thumbnail"), $("#static-content"));
+  });
+
+  $(".api-thumbnail").click(() => {
+    thumbnailSelect($("#api-thumbnail"), $("#api-content"));
+  });
+
+  $(".game-thumbnail").click(() => {
+    thumbnailSelect($("#game-thumbnail"), $("#game-content"));
+  });
+
+  //open contact modal
+  $(".contact-me").click(() => {
+    $("#contactModal").modal("toggle");
+  });
+
+  //Functions
+  let thumbnailSelect = (thumbnail, content) => {
+    $(".thumbnail").each(function () {
+      if ($(this)[0].id === thumbnail[0].id) {
+        $(this).addClass("selected");
+      } else {
+        $(this).removeClass("selected");
       }
     });
 
-    $("div.side-nav-layout").height(maxHeight + 20);
-  }
-
-  resizeDiv();
-
-  $(window).resize(function () {
-    resizeDiv();
-  });
+    $(".thumbnail-content").each(function () {
+      if ($(this)[0].id === content[0].id) {
+        $(this).addClass("show");
+      } else {
+        $(this).removeClass("show");
+      }
+    });
+  };
 });
