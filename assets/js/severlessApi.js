163a54a8f129ea0860006baebe4d70761348dc45
Nick Snyder <nsnyder1992@gmail.com>
2020-12-11 11:07:07 -0500

    added auto-update time left

--- assets/js/severlessApi.js
+++ assets/js/severlessApi.js
@@ -1,206 +1,210 @@
-$(document).ready(() => {
-  //urls
-  const corsProxy = "https://cors-anywhere.herokuapp.com/";
-  const baseUrl =
-    "https://iot-portfolio-app.azurewebsites.net/api/HttpTrigger1?code=l72Ca3JksC7iCbCGzIgDIAVLbHi57D1ub1vp81Hk0T9oXGgJAdwAnw==";
+//urls
+const corsProxy = "https://cors-anywhere.herokuapp.com/";
+const baseUrl =
+  "https://iot-portfolio-app.azurewebsites.net/api/HttpTrigger1?code=l72Ca3JksC7iCbCGzIgDIAVLbHi57D1ub1vp81Hk0T9oXGgJAdwAnw==";
 
-  //api queries
-  const lastUpdate = "&lastUpdate=true";
+//api queries
+const lastUpdate = "&lastUpdate=true";
 
-  //get insert point into document
-  const devInsert = document.getElementById("device-insert");
-  const azureBtn = $("#azure-btn");
-  const cloud = $("#azure-cloud");
+//get insert point into document
+const devInsert = document.getElementById("device-insert");
+const azureBtn = $("#azure-btn");
+const cloud = $("#azure-cloud");
 
-  //onClick
-  azureBtn.click(() => checkRefresh());
+//onClick
+azureBtn.click(() => checkRefresh());
 
-  //parameters
-  //date refresh and multiple of 10min refresh
-  let date = new Date();
-  let storageDate = new Date(localStorage.getItem("date")) || new Date();
+//parameters
+//date refresh and multiple of 10min refresh
+let date = new Date();
+let storageDate = new Date(localStorage.getItem("date")) || new Date();
 
-  //first visit refresh
-  let canRefresh = localStorage.getItem("refresh") === "true" || true;
+//first visit refresh
+let canRefresh = localStorage.getItem("refresh") === "true" || true;
 
-  //get local storage of devices
-  let devices = localStorage.getItem("devices") || "";
+//get local storage of devices
+let devices = localStorage.getItem("devices") || "";
 
-  //fetch the last sensor data
-  let getLastUpdate = async () => {
-    //remove children
-    cloud.remove();
-    while (devInsert.firstChild) {
-      devInsert.removeChild(devInsert.firstChild);
-    }
+//fetch the last sensor data
+let getLastUpdate = async () => {
+  //remove children
+  cloud.remove();
+  while (devInsert.firstChild) {
+    devInsert.removeChild(devInsert.firstChild);
+  }
 
-    //loading
-    const loading = document.createElement("div");
-    const srOnly = document.createElement("span");
-    loading.className = "spinner-border text-secondary";
-    loading.role = "status";
-    loading.id = "azure-loading";
-    srOnly.className = "sr-only";
-    srOnly.innerText = "Loading...";
+  //loading
+  const loading = document.createElement("div");
+  const srOnly = document.createElement("span");
+  loading.className = "spinner-border text-secondary";
+  loading.role = "status";
+  loading.id = "azure-loading";
+  srOnly.className = "sr-only";
+  srOnly.innerText = "Loading...";
 
-    loading.appendChild(srOnly);
-    devInsert.appendChild(loading);
+  loading.appendChild(srOnly);
+  devInsert.appendChild(loading);
 
-    //fetch
-    await fetch(corsProxy + baseUrl + lastUpdate)
-      .then((res) => res.json())
-      .then((json) => {
-        json = JSON.stringify(json);
-        displayDevices(json);
-        localStorage.setItem("time", Date.now());
-      })
-      .catch((err) => {
-        console.log(err);
+  //fetch
+  await fetch(corsProxy + baseUrl + lastUpdate)
+    .then((res) => res.json())
+    .then((json) => {
+      json = JSON.stringify(json);
+      displayDevices(json);
+      localStorage.setItem("time", Date.now());
+    })
+    .catch((err) => {
+      console.log(err);
 
-        //on fail clean up loading and append cloud
-        while (devInsert.firstChild) {
-          devInsert.removeChild(devInsert.firstChild);
-        }
-        $("device-insert").appendChild(cloud);
-      });
-  };
+      //on fail clean up loading and append cloud
+      while (devInsert.firstChild) {
+        devInsert.removeChild(devInsert.firstChild);
+      }
+      $("device-insert").append(cloud);
+    });
+};
 
-  //display devices
-  function displayDevices(devices) {
-    //remove children
-    cloud.remove();
-    while (devInsert.firstChild) {
-      devInsert.removeChild(devInsert.firstChild);
-    }
-    //set devices
-    localStorage.setItem("devices", devices);
+//display devices
+function displayDevices(devices) {
+  //remove children
+  cloud.remove();
+  while (devInsert.firstChild) {
+    devInsert.removeChild(devInsert.firstChild);
+  }
+  //set devices
+  localStorage.setItem("devices", devices);
 
-    //if not object parse JSON to javascript object
-    if (!(devices instanceof Object)) devices = JSON.parse(devices);
+  //if not object parse JSON to javascript object
+  if (!(devices instanceof Object)) devices = JSON.parse(devices);
 
-    //get timestamp
-    let devTime = devices.time;
+  //get timestamp
+  let devTime = devices.time;
 
-    //for each device create a card with device information
-    devices.devices.forEach((dev) => {
+  //for each device create a card with device information
+  devices.devices.forEach((dev) => {
+    //create html elements
+    const deviceContent = document.createElement("div");
+    const card = document.createElement("div");
+    const cardBody = document.createElement("div");
+    const header = document.createElement("h1");
+    const sensorList = document.createElement("div");
+
+    //set css classes
+    deviceContent.className = "device-data";
+    card.className = "card";
+    cardBody.className = "card-header";
+    header.className = "device-name lead";
+    sensorList.className = "list-group list-group-flush";
+
+    //set name of device to header of card
+    header.innerText = dev.name;
+
+    //append card children
+    cardBody.appendChild(header);
+    card.appendChild(cardBody);
+    card.appendChild(sensorList);
+    deviceContent.appendChild(card);
+
+    //for each sensor create a list item and display data
+    dev.sensors.forEach((sensor) => {
       //create html elements
-      const deviceContent = document.createElement("div");
-      const card = document.createElement("div");
-      const cardBody = document.createElement("div");
-      const header = document.createElement("h1");
-      const sensorList = document.createElement("div");
+      const sensorContent = document.createElement("div");
+      const para = document.createElement("p");
+      const pTime = document.createElement("p");
+      const span = document.createElement("span");
 
       //set css classes
-      deviceContent.className = "device-data";
-      card.className = "card";
-      cardBody.className = "card-header";
-      header.className = "device-name lead";
-      sensorList.className = "list-group list-group-flush";
+      sensorContent.className = "sensor-data list-group-item text-center";
+      para.className = "sensor-name lead";
+      pTime.className = "timestamp";
+      span.className = "sensor-value";
 
-      //set name of device to header of card
-      header.innerText = dev.name;
+      //text attributes
+      para.innerText = sensor.sensor;
+      pTime.innerText = "Timestamp: Today " + formatDate(devTime); //format date and return
+      span.innerText = sensor.value;
 
-      //append card children
-      cardBody.appendChild(header);
-      card.appendChild(cardBody);
-      card.appendChild(sensorList);
-      deviceContent.appendChild(card);
-
-      //for each sensor create a list item and display data
-      dev.sensors.forEach((sensor) => {
-        //create html elements
-        const sensorContent = document.createElement("div");
-        const para = document.createElement("p");
-        const pTime = document.createElement("p");
-        const span = document.createElement("span");
-
-        //set css classes
-        sensorContent.className = "sensor-data list-group-item text-center";
-        para.className = "sensor-name lead";
-        pTime.className = "timestamp";
-        span.className = "sensor-value";
-
-        //text attributes
-        para.innerText = sensor.sensor;
-        pTime.innerText = "Timestamp: Today " + formatDate(devTime); //format date and return
-        span.innerText = sensor.value;
-
-        //append sensor data to card
-        sensorContent.appendChild(para);
-        sensorContent.appendChild(span);
-        sensorContent.appendChild(pTime);
-        card.appendChild(sensorContent);
-      });
-
-      //insert devices to specified point
-      devInsert.appendChild(deviceContent);
+      //append sensor data to card
+      sensorContent.appendChild(para);
+      sensorContent.appendChild(span);
+      sensorContent.appendChild(pTime);
+      card.appendChild(sensorContent);
     });
-  }
 
-  //format timestamp on sensor data
-  function formatDate(date, timeOfDay = true) {
-    let d = new Date(date);
-    let hour =
-      d.getHours() > 12
-        ? d.getHours() - 12
-        : d.getHours() == 0
-        ? 12
-        : d.getHours();
-    hour = hour < 10 ? "0" + hour : hour;
-    let min = d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes();
-    let sec = d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds();
-    let am_pm;
-    timeOfDay ? (am_pm = d.getHours() > 12 ? "pm" : "am") : (am_pm = "");
+    //insert devices to specified point
+    devInsert.appendChild(deviceContent);
+  });
+}
 
-    return hour + ":" + min + ":" + sec + am_pm;
-  }
+//format timestamp on sensor data
+function formatDate(date, timeOfDay = true) {
+  let d = new Date(date);
+  let hour =
+    d.getHours() > 12
+      ? d.getHours() - 12
+      : d.getHours() == 0
+      ? 12
+      : d.getHours();
+  hour = hour < 10 ? "0" + hour : hour;
+  let min = d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes();
+  let sec = d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds();
+  let am_pm;
+  timeOfDay ? (am_pm = d.getHours() > 12 ? "pm" : "am") : (am_pm = "");
 
-  function checkRefresh() {
-    //first visit refresh
-    canRefresh = localStorage.getItem("refresh") === "true";
+  return hour + ":" + min + ":" + sec + am_pm;
+}
 
-    //date refresh and multiple of 10min refresh
-    storageDate = new Date(localStorage.getItem("date")) || new Date();
-    date = new Date();
-    let dateRefresh = date >= storageDate;
+function checkRefresh() {
+  //first visit refresh
+  canRefresh = localStorage.getItem("refresh") === "true";
 
-    //if refresh send request if not display local storage
-    if (canRefresh || dateRefresh) {
-      //set refresh to false
-      localStorage.setItem("refresh", false);
+  //date refresh and multiple of 10min refresh
+  storageDate = new Date(localStorage.getItem("date")) || new Date();
+  date = new Date();
+  let dateRefresh = date >= storageDate;
 
-      //format next available refresh date
-      date.setMinutes(Math.floor(date.getMinutes() / 10) * 10 + 10);
-      date.setSeconds(30);
+  //if refresh send request if not display local storage
+  if (canRefresh || dateRefresh) {
+    //set refresh to false
+    localStorage.setItem("refresh", false);
 
-      //create a string containing next date
-      let dateStr = date.toDateString();
-      dateStr += " " + formatDate(date, false);
+    //format next available refresh date
+    date.setMinutes(Math.floor(date.getMinutes() / 10) * 10 + 10);
+    date.setSeconds(30);
 
-      //store date for later
-      localStorage.setItem("date", dateStr);
+    //create a string containing next date
+    let dateStr = date.toDateString();
+    dateStr += " " + formatDate(date, false);
 
-      //get update
-      getLastUpdate();
+    //store date for later
+    localStorage.setItem("date", dateStr);
 
-      //update next available refresh date
-      $("#azure-min").text(minsLeft());
-    } else {
-      $("#azure-min").text(minsLeft());
-    }
-  }
+    //get update
+    getLastUpdate();
 
-  let minsLeft = () => {
-    let mins = Math.floor((storageDate - date) / 60000);
-    mins = mins < 0 ? "Refresh now!" : mins < 1 ? "<1min" : mins + "mins";
-    return mins;
-  };
-
-  //delete cloud on load if devices
-  if (devices !== "") {
-    displayDevices(devices);
+    //update next available refresh date
     $("#azure-min").text(minsLeft());
   } else {
     $("#azure-min").text(minsLeft());
   }
-});
+}
+
+let minsLeft = () => {
+  date = new Date();
+  let mins = Math.floor((storageDate - date) / 60000);
+  mins = mins < 0 ? "Refresh now!" : mins < 1 ? "<1min" : mins + "mins";
+  console.log(mins);
+  return mins;
+};
+
+//delete cloud on load if devices
+if (devices !== "") {
+  displayDevices(devices);
+  $("#azure-button").text(minsLeft());
+} else {
+  $("#azure-min").text(minsLeft());
+}
+
+//auto-update mins left
+$("#azure-min").text(minsLeft());
+window.setInterval(() => $("#azure-min").text(minsLeft()), 30000);
